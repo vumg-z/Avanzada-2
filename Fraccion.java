5076d64a98fa78df57f07c71fbbaeb9af7c72a95
vumg <ab17100222@gmail.com>
2024-08-28 12:50:01 -0600

    Create Fraccion.java

---
+++ Fraccion.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fraccion;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Fraccion {
+    public double numerador;
+    public double denominador;
+    public double entero;
+    
+    public Fraccion(double numerador, double denominador, double entero){
+        
+        //numerador (numerador = 1) ? this numerador=0 : this.numerador = numerador;
+    }
+    
+}
